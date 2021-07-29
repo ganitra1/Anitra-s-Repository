74f6ac4be94f7e2d0780afa8c5638371ac2f6b85
Anitra <griffinanitraa@johndeere.com>
2021-07-29 15:20:22 -0500

    day 1

---
+++ Game/script.js
@@ -0,0 +1,9 @@
+//create boxes and style them//
+//make buttons flash or move when sequence starts and when clicked on//
+//add sounds to each button//
+//make start button//
+//compter sequences//
+//human sequences//
+//restart after game //
+// you lose message if not successful//
+
